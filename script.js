ab19f49513c4b25934034d40f548673cdd5dc925
Favour Adebisi <adebisimomoluwa@gmail.com>
2024-06-12 20:12:26 +0100

    update index.html and script.js

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-let eyeicon = document.getElementById('eyeicon')
-// const button = document.getElementById('button')
-
 function login(){
     let password = document.getElementById('password').value
     let matricNo = document.getElementById('matricNo').value
     let matricPass = document.getElementById('matricPass')
     let matricPass2 = document.getElementById('matricPass2')
-    if (password ==="" || matricNo ==="" || matricNo.length > 10 || matricNo.length < 10){
+    let matricPattern = /^2023/;
+    if (password ==="" || matricNo ==="" || matricNo.length > 10 || matricNo.length < 10 || password > 14){
         matricPass.style.display='block';
         matricPass2.style.display='block' ;
-    }else{
+    }else if (!matricPattern.test(matricNo)){
+        matricPass.innerHTML=``
+    } else{
         console.log("You're in danger")
     }
 }
-eyeicon.addEventListener('click', (e)=>{
-    document.getElementById('eyeiconspan').innerHTML=`<i class="fa-solid fa-eye-slash pt-4" onclick='eyeicon2'></i>`
-    
-})
+function eyeicon(){
+    document.getElementById('eyeiconspan').innerHTML=`<i class="fa-solid fa-eye-slash pt-4" onclick='eyeicon2()'></i>`
+    // document.getElementById('eyeiconspan').innerHTML=''
+}
 
-// let eyeicon2 =document.getElementById('eyeicon2')
-// function eyeicon2(){
-//     document.getElementById('eyeiconspan').innerHTML=`<i class="fa-solid fa-eye pt-4" id="eyeicon"></i>`
-// }
-
-
-
-// button.addEventListener('click', (e)=>{
-//     if (password ==="" || matricNo ===""){
-//         alert("This spaces can't be empty")
-//     }
-// })
+function eyeicon2(){
+    document.getElementById('eyeiconspan').innerHTML=`<i class="fa-solid fa-eye pt-4" id="eyeicon"></i>`
+}
+eyeicon()
