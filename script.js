8fd27c30880f7999ad55cdf6a9573f148866c001
Favour Adebisi <adebisimomoluwa@gmail.com>
2024-06-12 22:04:02 +0100

    update index.html, script.js and style.css

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function login(){
+    button.innerText=`Processing...` 
+    setTimeout(() => {
+        button.innerHTML="Login"
+    }, 2000);
     let password = document.getElementById('password').value
     let matricNo = document.getElementById('matricNo').value
+    let matricPass1 = document.getElementById('matricPass1')
     let matricPass = document.getElementById('matricPass')
     let matricPass2 = document.getElementById('matricPass2')
     let matricPattern = /^2023/;
@@ -8,17 +13,23 @@ function login(){
         matricPass.style.display='block';
         matricPass2.style.display='block' ;
     }else if (!matricPattern.test(matricNo)){
-        matricPass.innerHTML=``
+        matricPass1.style.display='block'
     } else{
+        matricPass.style.display='none';
+        matricPass2.style.display='none';
+        matricPass1.style.display='none'
         console.log("You're in danger")
+
     }
 }
+let passwordfield= document.getElementById('password').value
 function eyeicon(){
     document.getElementById('eyeiconspan').innerHTML=`<i class="fa-solid fa-eye-slash pt-4" onclick='eyeicon2()'></i>`
+    const type = pass
     // document.getElementById('eyeiconspan').innerHTML=''
 }
 
 function eyeicon2(){
-    document.getElementById('eyeiconspan').innerHTML=`<i class="fa-solid fa-eye pt-4" id="eyeicon"></i>`
+    document.getElementById('eyeiconspan').innerHTML=`<i class="fa-solid fa-eye pt-4" onclick="eyeicon()"></i>`
 }
-eyeicon()
+
