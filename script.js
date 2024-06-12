c6386f1ae30241f2fd5d0ed79c83317e0088ce93
Favour Adebisi <adebisimomoluwa@gmail.com>
2024-06-12 16:38:26 +0100

    update index.html, script.js and style.css

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 let eyeicon = document.getElementById('eyeicon')
-const button = document.getElementById('button')
+// const button = document.getElementById('button')
+
+function login(){
+    let password = document.getElementById('password').value
+    let matricNo = document.getElementById('matricNo').value
+    if (password ==="" || matricNo ==="" || matricNo.length > 10){
+        matricPass.style.display='block'; 
+    }else{
+        console.log("You're in danger")
+    }
+}
 eyeicon.addEventListener('click', (e)=>{
     document.getElementById('eyeiconspan').innerHTML=`<i class="fa-solid fa-eye-slash pt-4" onclick='eyeicon2'></i>`
     
 })
 
 // let eyeicon2 =document.getElementById('eyeicon2')
-function eyeicon2(){
-    document.getElementById('eyeiconspan').innerHTML=`<i class="fa-solid fa-eye pt-4" id="eyeicon"></i>`
-}
+// function eyeicon2(){
+//     document.getElementById('eyeiconspan').innerHTML=`<i class="fa-solid fa-eye pt-4" id="eyeicon"></i>`
+// }
 
 
 
-button.addEventListener('click', (e)=>{
-    if (password ==="" || matricNo ===""){
-        alert("This spaces can't be empty")
-    }
-})
-
-function button(){
-    if (password ==="" || matricNo ===""){
-        alert("This spaces can't be empty")
-    }
-}
+// button.addEventListener('click', (e)=>{
+//     if (password ==="" || matricNo ===""){
+//         alert("This spaces can't be empty")
+//     }
+// })
