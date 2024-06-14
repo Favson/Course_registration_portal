774772fea35c079645f5be68268935adf65a1beb
Favour Adebisi <adebisimomoluwa@gmail.com>
2024-06-14 09:52:36 +0100

    update home.html, index.html, script.js and style.css

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-let studentMatric =[2023008625, 2023007271, 2023006202, 2023007238, 2023002857,2023008028,]
+let studentMatric =[2023008625, 2023007271, 2023006202, 2023007238, 2023002857,2023008028, 2023010703,]
+
+//The Login Button
 function login(){
     button.innerText=`Processing...` 
     setTimeout(() => {
         button.innerHTML="Login"
-    }, 2000);
+    }, 1000);
     let password = document.getElementById('password').value
     let matricNo = document.getElementById('matricNo').value
     let matricPass1 = document.getElementById('matricPass1')
     let matricPass = document.getElementById('matricPass')
     let matricPass2 = document.getElementById('matricPass2')
     let matricPattern = /^2023/;
+
     if (password.value ==="" || matricNo ==="" || matricNo.length > 10 || matricNo.length < 10 || password.value > 14){
         matricPass.style.display='block';
         matricPass2.style.display='block';
         matricPass1.style.display='none'
         document.getElementById('password').value=""
-    }else if (!matricPattern.test(matricNo) || studentMatric){
+    }else if (!matricPattern.test(matricNo) || !studentMatric){
         document.getElementById('password').value=""
         matricPass1.style.display='block'
         matricPass.style.display='none';
         matricPass2.style.display='none';
     } else{
+        document.getElementById('loading').style.display='block'
+        setTimeout(() => {
+            document.getElementById('loading').style.display='none'
+        }, 2000);
         matricPass.style.display='none';
         matricPass2.style.display='none';
         matricPass1.style.display='none'
-        items.push(matricNo.value)
-        console.log("You're in danger")
-
+        studentMatric.push(matricNo.value)
+        localStorage.setItem('studentVar', JSON.stringify(studentMatric))
+        window.location.href = "home.html"
     }
 }
 document.getElementById('eyeiconspan').addEventListener('click', function () {
@@ -58,3 +65,7 @@ function resetCode() {
     emailError.style.display='none'
    }
 }
+
+
+//Homepage Scripts
+
