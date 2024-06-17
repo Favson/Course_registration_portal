6bbd96a631f784cb0d6488f1dac03edee2317281
Favour Adebisi <adebisimomoluwa@gmail.com>
2024-06-17 18:15:40 +0100

    update home.html and script.js

---
+++ hfh.py
@@ -0,0 +1,13 @@
+# print('Hi there, welcome to Favson college')
+# User_Name = input('Input your name:-  ').strip()
+# print("It's a priviledge to have you with us", User_Name, "Kindly proceed to completing the form below")
+# User_Name = float(input("'How Old are you? :"))
+# User_Name= str(input('Do you enjoy coding? :'))
+# User_Name = input('Nationality :')
+# User_Name = input('State :-')
+
+#Code For Calculating the Area of a circle
+radius =float(input('input the radius of the circle:-'))
+print("The Area of a circle = ",3.142*radius**2)
+
+
