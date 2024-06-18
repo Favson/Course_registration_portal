7c2bb386dda346cab58e49ddffd6d67ab36bbd24
Favour Adebisi <adebisimomoluwa@gmail.com>
2024-06-19 00:09:35 +0100

    delete 1 file and update 2 files

--- script2.js
+++ script2.js
@@ -1,24 +1,3 @@
-if (matricNo.value == 2023008625) {
-    document.getElementById('studentName1').innerHTML='ADEBISI FAVOUR MOMOLUWA';
-    //studentMatricNo.innerHTML = matricNo
-    
-    //console.log("I Am djn");
-}
-
-const studentData = {
-    "2023007271": "AFOLABI JOSEPH ENIOLA",
-    "2023008625": "ADEBISI FAVOUR MOMOOLUWA",
-    // add more matric numbers and names here
-  };
-  
-  //...
-  
-  if (studentData[matricNo]) {
-    studentName1.innerHTML = studentData[matricNo];
-    studentMatricNo.innerHTML = matricNo
-    console.log("I Am djn");
-}
-
 
 let slideOut = document.getElementById('slideOut')
 function arrowDiv() {
