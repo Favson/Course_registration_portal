81d48285440d7ecd8bef66dc3af2b95bf66ab31c
Favour Adebisi <adebisimomoluwa@gmail.com>
2024-06-22 13:09:11 +0100

    update adeolaSummary.html, script2.js and summary.html

--- script2.js
+++ script2.js
@@ -14,7 +14,6 @@ checkboxes.forEach(checkbox => {
 });
 
 let studentMatric =[2023008625, 2023007271, 2023006202, 2023007238, 2023002857,2023008028, 2023010703, 2023004212, 2023002427, 2023002524, 2023010465, 2023011681, 2023003139, 2023008891, 2023008239, 2023011299, 2023004947, 2023005814]
-let UserArray=[]
 function registerCourses() {
     let inputName = document.getElementById('inputName').value;
     let matricNumber = document.getElementById("matricNumber").value;
@@ -29,10 +28,6 @@ function registerCourses() {
     }else if(document.getElementById('checkBox').checked.length =  9){
         document.getElementById('incorrect').style.display='none'
         document.getElementById('err').style.display='none'
-        let UserObj = {inputName, matricNumber}
-        UserArray.push(UserObj)
-        localStorage.setItem('username', JSON.stringify(UserArray));
-
         const form = document.getElementById('registrationForm');
         const selectedCourses = Array.from(form.elements['course'])
                                     .filter(course => course.checked)
@@ -47,17 +42,5 @@ function registerCourses() {
     checkboxes.forEach(checkbox => checkbox.checked = false);
     document.getElementById('inputName').value=''
     document.getElementById('matricNumber').value="" 
-    count=""
-    document.getElementById('inputName').value=''
-    document.getElementById('matricNumber').value="" 
-    document.getElementById('UserNameOut').innerHTML ="";
-    document.getElementById('matricNoOut').innerHTML ="";
+    count="" 
 }
- UserArray = JSON.parse(localStorage.getItem("username")) || [];
- 
- if (UserArray.length > 1) {
-     document.getElementById('UserNameOut').innerHTML = `${UserArray.inputName}`;
-     document.getElementById('matricNoOut').innerHTML = `${UserArray.matricNumber}`;
-     console.log(UserArray)
-     registerCourses()
- }
