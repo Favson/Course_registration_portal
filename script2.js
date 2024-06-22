8912c626bb940155040130bc05b6efbcdf19e41a
Favour Adebisi <adebisimomoluwa@gmail.com>
2024-06-22 14:05:10 +0100

    delete 2 files and update 2 files

--- script2.js
+++ script2.js
@@ -36,8 +36,38 @@ function registerCourses() {
                                 
     if(matricNumber == 2023008625 || inputName == "ADEBISI FAVOUR MOMOLUWA"){
         window.location.href = 'summary.html';
-    }else if (matricNumber === 2023004212 || inputName == "ADEBOWALE ADEOLA PRECIOUS"){
+    }else if (matricNumber == 2023004212 || inputName == "ADEBOWALE ADEOLA PRECIOUS"){
         window.location.href='adeolaSummary.html '
+    }else if (inputName == 'ADEBISI ISHAQ AYOMIDE'|| matricNumber == 2023002857){
+        window.location.href='Adebisi ishaqresult.html'
+    }else if(inputName == 'Adebisi olaide Ezekiel' || matricNumber == 2023011681){
+        window.location.href='Adebisi OlaideResult.html'
+    }else if (inputName == 'Adebowale victo Oladayo' || matricNumber ==2023010703){
+        window.location.href='Adebowale victo OladayoResult.html'
+    }else if (inputName == 'Adedeji Micheal Adewale' || matricNumber == 2023011681){
+        window.location.href='AdedejiReult.html'
+    }else if (inputName == 'Adediji Victor Oluwabukunmi' || matricNumber == 2023010465){
+        window.location.href='Adediji victorRe.html'
+    }else if (inputName == 'Adedokun Elijah Adewale' || matricNumber == 2023008028){
+        window.location.href='Adedokun elijahResult.html'
+    }else if (inputName == 'Adegun Temiloluwa Samuel' || matricNumber == 2023011299){
+        window.location.href='Adegun Temiloluwareslut.html'
+    }else if (inputName == 'Adekoya Anjolaoluwa Abdulahmid' || matricNumber == 2023008891){
+        window.location.href='Adekoya Anjolaoluwaresly.html'
+    }else if (inputName == 'Adekunjo Emmanuel Ayomide' || matricNumber == 2023002524){
+        window.location.href='Adekunjo Emmanuel AyomideReslt.html'
+    }else if (inputName == 'Adekunle Abdulhameed Ademola' || matricNumber == 2023005814){
+        window.location.href='Adekunle Abdulhameedresult.html'
+    }else if (inputName == 'Adekunle Glory olamide' || matricNumber == 2023003139){
+        window.location.href='Adekunle Glryresult.html'
+    }else if (inputName == 'Adelaja Abraham Ayomide' || matricNumber ==2023007238){
+        window.location.href='Adelaja AbrahamResult.html'
+    }else if (inputName == 'Adelakun Ayobami Grace' || matricNumber == 2023007271){
+        window.location.href='AdelakunGraceResult.html'
+    }else if (inputName == 'ADELEYE, Oluwaferanmi Promise' || matricNumber ==2023004947){
+        window.location.href='Adeleye,oluwaferanmiResult.html'
+    }else if (inputName == 'Adelusi Desmond' || matricNumber == 2023006202){
+        window.location.href='Adelusi DesmondResult.html'
     }
     checkboxes.forEach(checkbox => checkbox.checked = false);
     document.getElementById('inputName').value=''
