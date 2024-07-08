e09e3fb5b1d05542a02193886e54f2a0d5d2983f
Favour Adebisi <adebisimomoluwa@gmail.com>
2024-07-08 14:54:02 +0100

    updated files

--- script2.js
+++ script2.js
@@ -1,7 +1,10 @@
+let details = []
+//Calling out the id needed and saving them in a new variable
 let display = document.getElementById('display')
 let checkBox = document.getElementById('checkBox')
 let checkboxes = document.querySelectorAll('input[type="checkbox"]')
-let count = 0;        
+let count = 0;  
+//For the checkbox to count all the box checked       
 checkboxes.forEach(checkbox => {
     checkbox.addEventListener('change', () => {
         if (checkbox.checked) {
@@ -13,8 +16,8 @@ checkboxes.forEach(checkbox => {
     });
 });
 
-let details = []
 let studentMatric =[2023008625, 2023007271, 2023006202, 2023007238, 2023002857,2023008028, 2023010703, 2023004212, 2023002427, 2023002524, 2023010465, 2023011681, 2023003139, 2023008891, 2023008239, 2023011299, 2023004947, 2023005814]
+//Function to know the registered courses and to make sure it's 9 courses registered 
 function registerCourses() {
     let inputName = document.getElementById('inputName').value;
     let matricNumber = document.getElementById("matricNumber").value;
@@ -27,14 +30,16 @@ function registerCourses() {
     }else if (count < 9){
         alert('All Must be checked')
     }else if(document.getElementById('checkBox').checked.length =  9){
-        details.push(inputName, matricNumber)
+        details.push(inputName.value, matricNumber.value)
         localStorage.setItem("studentDetails", JSON.stringify(details))
+        window.location.href='sum.html'
         document.getElementById('incorrect').style.display='none'
         document.getElementById('err').style.display='none'
         const form = document.getElementById('registrationForm');
         const selectedCourses = Array.from(form.elements['course'])
                                     .filter(course => course.checked)
                                     .map(course => course.value);
+        alert("Congratulations! You've been registered successfully!")
     }
                                 
     if(matricNumber == 2023008625 || inputName == "ADEBISI FAVOUR MOMOLUWA"){
@@ -80,5 +85,3 @@ function registerCourses() {
     count="" 
 }
 
-details = JSON.parse(localStorage.getItem("studentDetails")) || [];
-console.log(details);
